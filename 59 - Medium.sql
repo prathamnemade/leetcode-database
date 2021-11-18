71f71e0eb742c331e9973da73996f2b26a5dab61
prathamnemade <30521872+prathamnemade@users.noreply.github.com>
2021-11-18 12:05:10 -0500

    Movie Rating - 59
    -- Question 59
    -- Table: Movies
    
    -- +---------------+---------+
    -- | Column Name   | Type    |
    -- +---------------+---------+
    -- | movie_id      | int     |
    -- | title         | varchar |
    -- +---------------+---------+
    -- movie_id is the primary key for this table.
    -- title is the name of the movie.
    -- Table: Users
    
    -- +---------------+---------+
    -- | Column Name   | Type    |
    -- +---------------+---------+
    -- | user_id       | int     |
    -- | name          | varchar |
    -- +---------------+---------+
    -- user_id is the primary key for this table.
    -- Table: Movie_Rating
    
    -- +---------------+---------+
    -- | Column Name   | Type    |
    -- +---------------+---------+
    -- | movie_id      | int     |
    -- | user_id       | int     |
    -- | rating        | int     |
    -- | created_at    | date    |
    -- +---------------+---------+
    -- (movie_id, user_id) is the primary key for this table.
    -- This table contains the rating of a movie by a user in their review.
    -- created_at is the user's review date. 
     
    
    -- Write the following SQL query:
    
    -- Find the name of the user who has rated the greatest number of the movies.
    -- In case of a tie, return lexicographically smaller user name.
    
    -- Find the movie name with the highest average rating in February 2020.
    -- In case of a tie, return lexicographically smaller movie name.
    
    -- Query is returned in 2 rows, the query result format is in the folowing example:
    
    -- Movies table:
    -- +-------------+--------------+
    -- | movie_id    |  title       |
    -- +-------------+--------------+
    -- | 1           | Avengers     |
    -- | 2           | Frozen 2     |
    -- | 3           | Joker        |
    -- +-------------+--------------+
    
    -- Users table:
    -- +-------------+--------------+
    -- | user_id     |  name        |
    -- +-------------+--------------+
    -- | 1           | Daniel       |
    -- | 2           | Monica       |
    -- | 3           | Maria        |
    -- | 4           | James        |
    -- +-------------+--------------+
    
    -- Movie_Rating table:
    -- +-------------+--------------+--------------+-------------+
    -- | movie_id    | t      | rating       | created_at  |
    -- +-------------+--------------+--------------+-------------+
    -- | 1           | 1            | 3            | 2020-01-12  |
    -- | 1           | 2            | 4            | 2020-02-11  |
    -- | 1           | 3            | 2            | 2020-02-12  |
    -- | 1           | 4            | 1            | 2020-01-01  |
    -- | b           | 1            | 5            | 2020-02-17  | 
    -- | 2           | 2            | 2            | 2020-02-01  | 
    -- | 2           | 3            | 2            | 2020-03-01  |
    -- | 3           | 1            | 3            | 2020-02-22  | 
    -- | 3           | 2            | 4            | 2020-02-25  | 
    -- +-------------+--------------+--------------+-------------+
    
    -- Result table:
    -- +--------------+
    -- | results      |
    -- +--------------+
    -- | Daniel       |
    -- | Frozen 2     |
    -- +--------------+
    
    -- Daniel and Maria have rated 3 movies ("Avengers", "Frozen 2" and "Joker") but Daniel is smaller lexicographically.
    -- Frozen 2 and Joker have a rating average of 3.5 in February but Frozen 2 is smaller lexicographically.

---
+++ 59 - Medium.sql
@@ -0,0 +1,30 @@
+-- Question 59
+
+Create table If Not Exists Movies (movie_id int, title varchar(30));
+Create table If Not Exists Users (user_id int, name varchar(30));
+Create table If Not Exists MovieRating (movie_id int, user_id int, rating int, created_at date);
+Truncate table Movies;
+insert into Movies (movie_id, title) values ('1', 'Avengers');
+insert into Movies (movie_id, title) values ('2', 'Frozen 2');
+insert into Movies (movie_id, title) values ('3', 'Joker');
+Truncate table Users;
+insert into Users (user_id, name) values ('1', 'Daniel');
+insert into Users (user_id, name) values ('2', 'Monica');
+insert into Users (user_id, name) values ('3', 'Maria');
+insert into Users (user_id, name) values ('4', 'James');
+Truncate table MovieRating;
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '1', '3', '2020-01-12');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '2', '4', '2020-02-11');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '3', '2', '2020-02-12');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('1', '4', '1', '2020-01-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '1', '5', '2020-02-17');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '2', '2', '2020-02-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('2', '3', '2', '2020-03-01');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '1', '3', '2020-02-22');
+insert into MovieRating (movie_id, user_id, rating, created_at) values ('3', '2', '4', '2020-02-25');
+
+with modified_table as (select avg(mt.rating) as average, mt.title from (select m.movie_id as movie_id, m.rating as rating, m.created_at as created_at, mm.title as title from MovieRating m inner join Movies mm on m.movie_id = mm.movie_id order by mm.title ) as mt where month(mt.created_at) = 2 group by mt.movie_id, mt.title)
+(select mr.name as show1 from (select distinct m.movie_id as movie_id, m.user_id as user_id, m.created_at as created_at, u.name as name from MovieRating as m inner join Users as u on m.user_id = u.user_id order by u.name ) as mr group by mr.user_id,mr.name limit 1)
+union
+(select title as show1 from modified_table order by average desc limit 1);
+
